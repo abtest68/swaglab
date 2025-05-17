d1782bbe9928daa92a891f05811ebe884ce36e3d
Abhishek Mallah <abhimallah68@gmail.com>
2025-05-17 13:05:59 +0530

    Added 1 TC

---
+++ src/test/java/checkBox.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class checkBox {
+    @Test(description = "CheckBoxDemo")
+    public void check() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/checkbox");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+        WebElement element = driver.findElement(By.cssSelector(".rct-checkbox svg"));
+        boolean isDisplayed = element.isDisplayed();
+        boolean isSelected = element.isSelected();
+        boolean isEnabled = element.isEnabled();
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].scrollIntoView()",element);
+
+        if (isDisplayed == true && isSelected == false && isEnabled == true) {
+            element.click();
+            System.out.println("Clicked");
+        }else {
+            System.out.println("Not clickable");
+        }
+
+        driver.quit();
+
+    }
+}
