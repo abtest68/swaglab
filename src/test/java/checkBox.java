ae8959707e2bbb1187534b668d459e324a0925fe
abtest68 <techabm66@gmail.com>
2025-05-17 20:43:05 +0530

    Update checkBox.java

--- src/test/java/checkBox.java
+++ src/test/java/checkBox.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.time.Duration;
 
